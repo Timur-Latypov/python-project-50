5b39ea2339af1834e725be86971caeaabac55645
Timur Latypov <350250431@mail.ru>
2023-08-26 18:04:06 +0500

    fix hexlet-check error 2

--- gendiff/formatters/plain_format.py
+++ gendiff/formatters/plain_format.py
@@ -45,5 +45,7 @@ def normalize(value):
         return 'null'
     elif isinstance(value, dict):
         return '[complex value]'
-    else:
+    elif isinstance(value, str):
         return f"'{value}'"
+    else:
+        return value
